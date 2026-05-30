7d9034f4fc128de4283b9361a1e41a1f9309eb5b
dimkasimka228 <dimapakhalovich@gmail.com>
2026-05-30 17:30:19 +0300

    add class-validator, configure global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable global validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
   
   // Додаємо || '3000', щоб уникнути помилки типу undefined
   const port = process.env.APP_PORT || '3000';
